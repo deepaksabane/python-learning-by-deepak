507a3cf96ace7d69e7fcd8420c7e1eb56eda877f
Sabane Deepak <deepak.sabane@peopletech.com>
2024-08-29 18:09:36 +0530

    added video18

---
+++ video18_whileloop.py
@@ -0,0 +1,17 @@
+###  Python while Loop  ###
+'''As the name suggests, while loops execute statements while the condition is True.
+As soon as the condition becomes False, the interpreter comes out of the while loop.'''
+
+
+i = 0 
+while(i<3): # in this program we know that i value is 0 less than 3 so it run in the below we have added
+    print(i)
+    i = i + 1 # o+1= 1 , 1+1=2 , 2+2=4 -  it will not execute 
+print("Done with the loop!!")
+    #in each iteration we are increasing the value , and it will check the condition 
+
+g = (int(input("Enter the number : ")))
+while(g <= 40):
+    g = int(input("Enter the number : "))
+    print(g)
+print(" Done with the loop ")
